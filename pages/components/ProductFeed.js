03c00be9300801cf1f3d47fec1cf13049c39dcf7
Naeem676 <ali.naeem939@gmail.com>
2021-08-03 11:14:13 +0600

    if true

--- pages/components/ProductFeed.js
+++ pages/components/ProductFeed.js
@@ -7,7 +7,7 @@ function ProductFeed({ products }) {
     return (
         <div className='grid grid-flow-row-dense bg-gray-300 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
             {
-                products.slice(0, 4).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
+                products?.slice(0, 4).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
                 id={id} title={title}  price={price} 
                 description={description} category={category}
                  image={image}  />)
@@ -18,14 +18,14 @@ function ProductFeed({ products }) {
             
             <div className='md:col-span-2'>
                 {
-                    products.slice(4, 5).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
+                    products?.slice(4, 5).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
                     id={id} title={title}  price={price} 
                     description={description} category={category}
                     image={image}  />)
                 }
             </div>
             {
-                products.slice(5, products.length).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
+                products?.slice(5, products.length).map(({id, _id, price,  title, description, category, image}) => <Product key={_id} 
                 id={id} title={title}  price={price} 
                 description={description} category={category}
                  image={image}  />)
