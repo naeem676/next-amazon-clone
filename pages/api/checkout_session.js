04d5df28360dc40a504d5f42776f829848828dd9
Naeem676 <ali.naeem939@gmail.com>
2021-08-05 16:07:13 +0600

    seccess

--- pages/api/checkout_session.js
+++ pages/api/checkout_session.js
@@ -24,7 +24,7 @@ export default  async function handler  (req, res) {
         },
         line_items:transfromedItems,
         mode:'payment',
-        success_url:`${process.env.HOST}/Success`,
+        success_url:`${process.env.HOST}/success`,
         cancel_url:`${process.env.HOST}/Checkout`,
         metadata:{
             email,
@@ -32,34 +32,4 @@ export default  async function handler  (req, res) {
         }
     });
     res.status(200).json({id:session.id});
-};
-
-// export default async function handler(req, res) {
-//   if (req.method === 'POST') {
-//     try {
-//       // Create Checkout Sessions from body params.
-//       const session = await stripe.checkout.sessions.create({
-//         payment_method_types: [
-//           'card',
-//         ],
-//         line_items: [
-//           {
-//             // TODO: replace this with the `price` of the product you want to sell
-//             price: '{{PRICE_ID}}',
-//             quantity: 1,
-//           },
-//         ],
-//         mode: 'payment',
-//         success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
-//         cancel_url: `${req.headers.origin}/?canceled=true`,
-//       });
-
-//       res.redirect(303, session.url);
-//     } catch (err) {
-//       res.status(err.statusCode || 500).json(err.message);
-//     }
-//   } else {
-//     res.setHeader('Allow', 'POST');
-//     res.status(405).end('Method Not Allowed');
-//   }
-// }
+}
