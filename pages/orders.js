8b3a11df00ab93682d105a53f59be6cb812f1f2f
Naeem676 <ali.naeem939@gmail.com>
2021-08-06 15:30:51 +0600

    length

--- pages/orders.js
+++ pages/orders.js
@@ -14,7 +14,7 @@ function Order({ orders }) {
                 <h1 className='text-3xl border-b mb-2 pb-1 border-yellow-400'>Your Orders</h1>
                 {
                 session ? (
-                    <h2>{orders ? orders.length : 'x'} Orders</h2>
+                    <h2>{orders ? orders?.length : 'x'} Orders</h2>
                 ) : (
                     <h2>Please sign in to see your orders</h2>
                 )
